2ef71c1f6aa5f04480327278a27a2ae76e27c7d4
bozkurthan <burakhancorak.ws@gmail.com>
2019-07-30 12:04:31 +0300

    Update TCP-Client.py

--- Examples/TCP-Client.py
+++ Examples/TCP-Client.py
@@ -4,14 +4,14 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('localhost', 1000)
+server_address = ('localhost', 10000)
 print('connecting to {} port {}'.format(*server_address))
 sock.connect(server_address)
 
 try:
 
     # Send data
-    message = 'This is the message.  It will be repeated.' #TODO something is wrong here
+    message = b'This is the message.  It will be repeated.'
     print('sending {!r}'.format(message))
     sock.sendall(message)
 
@@ -25,5 +25,5 @@ try:
         print('received {!r}'.format(data))
 
 finally:
-	print('closing socket')
-	sock.close()
+    print('closing socket')
+    sock.close()
