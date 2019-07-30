5b14c138ca5e4632db49ead3d91124dbcb426e4f
bozkurthan <burakhancorak.ws@gmail.com>
2019-07-30 09:08:51 +0300

    Python server code.
    Signed-off-by: bozkurthan <burakhancorak.ws@gmail.com>

--- Examples/TCP-Client.py
+++ Examples/TCP-Client.py
@@ -4,9 +4,9 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('localhost', 10000)
+server_address = ('localhost', 1000)
 print('connecting to {} port {}'.format(*server_address))
-sock.connect(server_ddress)
+sock.connect(server_address)
 
 try:
 
